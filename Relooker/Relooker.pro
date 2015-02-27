00aae2314d61045f8b5327bd548624e73157b377
Mixbo <mixbo@wakowakowako.com>
2015-02-27 12:49:26 -0500

    First Commit
    1st basic GUI with school documentation

---
+++ Relooker/Relooker.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-02-20T19:23:45
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Relooker
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ Relooker/mainwindow.cpp
@@ -0,0 +1,20 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_verticalTabsList_itemClicked(QListWidgetItem *item)
+{
+    int index = item->listWidget()->row( item );
+    ui->stackedEditor->setCurrentIndex(index);
+}
