1bcbb9e42de636b4036c76a663ef150155a6dfe3
Mixbo <mixbo@wakowakowako.com>
2015-06-08 12:56:11 -0400

    Executables and keys

---
+++ Analyse-Pres/Executable/executable.cpp
@@ -0,0 +1,8 @@
+#include "executable.h"
+
+int main()
+{
+    std::string key = "gIbtAaUFv7WGT/YnKbFX3K0QPXhuYHuta+kurX3Sa3m/+yzIM3ezdrNvdCecAlNgwIWurqqFpMzfm6j5vI76b/8AaVTO8bzTSZFv1Rp54sKq6zHHmlzWart";
+    std::cout << "Hello, world!\n";
+    return 0;
+}
